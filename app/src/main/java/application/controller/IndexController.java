d8d1f6522c94c0ad42a179dd866d060b88096d2f
felipemartins5222 <128558157+felipemartins5222@users.noreply.github.com>
2024-04-05 19:55:00 +0000

    Tabuada

--- app/src/main/java/application/controller/IndexController.java
+++ app/src/main/java/application/controller/IndexController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
 public class IndexController {
     @RequestMapping("/")
     public String home() {
-        return "WEB-INF/home.jsp";
+        return "home";
     }
 }
 
---
+++ app/src/main/java/application/controller/TabuadaController.java
@@ -0,0 +1,26 @@
+package appliction.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/tabuada")
+public class TabuadaController {
+    @RequestMapping("/form")
+    public String form() {
+        return "form";
+    }
+
+    @RequestMapping("/calcular")
+    public String calcular(@RequestParam("valor")int valor) {
+        int[] resultado = new int[10];
+        for(int num = 1;num <= 10; num++)
+            resultado[num-1] = num * valor;
+        ui.addAtribute("tabuada", resultado);
+        return "form";
+    }
+
+}
